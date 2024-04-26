d1c1433ca881cb8f4a3a8cd1dc65692269020996
ompharate31 <ompharate31@gmail.com>
2024-04-27 00:47:15 +0530

    domain changed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:"https://twitter-pqs5.vercel.app",
+    origin:"https://op-x.vercel.app",
     credentials:true
 }
 app.use(cors(corsOptions));
