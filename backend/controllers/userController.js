122da7abc9ae639167a95a3dde40bb6bae1efc20
ompharate31 <ompharate31@gmail.com>
2024-04-26 23:28:28 +0530

    test 3

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,38 +3,39 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const Register = async (req, res) => {
-    try {
-        const { name, username, email, password } = req.body;
-        // basic validation
-        if (!name || !username || !email || !password) {
-            return res.status(401).json({
-                message: "All fields are required.",
-                success: false
-            })
-        }
-        const user = await User.findOne({ email });
-        if (user) {
-            return res.status(401).json({
-                message: "User already exist.",
-                success: false
-            })
-        }
-        const hashedPassword = await bcryptjs.hash(password, 16);
+    res.send("resgister")
+    // try {
+    //     const { name, username, email, password } = req.body;
+    //     // basic validation
+    //     if (!name || !username || !email || !password) {
+    //         return res.status(401).json({
+    //             message: "All fields are required.",
+    //             success: false
+    //         })
+    //     }
+    //     const user = await User.findOne({ email });
+    //     if (user) {
+    //         return res.status(401).json({
+    //             message: "User already exist.",
+    //             success: false
+    //         })
+    //     }
+    //     const hashedPassword = await bcryptjs.hash(password, 16);
 
-        await User.create({
-            name,
-            username,
-            email,
-            password: hashedPassword
-        });
-        return res.status(201).json({
-            message: "Account created successfully.",
-            success: true
-        })
+    //     await User.create({
+    //         name,
+    //         username,
+    //         email,
+    //         password: hashedPassword
+    //     });
+    //     return res.status(201).json({
+    //         message: "Account created successfully.",
+    //         success: true
+    //     })
 
-    } catch (error) {
-        console.log(error);
-    }
+    // } catch (error) {
+    //     console.log(error);
+    // }
 }
 export const Login = async (req, res) => {
     try {
